df47ee74a2b8e2bbc623d3fbe1187b8b96dce8e4
Sylvester <sylvester.lindfors@gmail.com>
2020-04-30 14:19:40 +0200

    fixed so Page loop were Page acted as post

--- index.php
+++ index.php
@@ -1,38 +1,19 @@
     <!-- Header -->
-<?php
-        get_header();
+    <?php
+    get_header();
 ?>
-<div class="container p-4">
-
-
-<div class="d-flex">
-        <div class="col-md-9 content">
-        <!-- Posts Loop Start -->
-            <?php if ( have_posts() ) : ?>
-            <!-- Woop Woop, We haz posts -->
-            <div class="card-group">
-                <div class="row">
-                    <?php while ( have_posts() ) : the_post(); ?>
-                        <?php get_template_part('template-parts/content' ,'excerpt'); ?>
-                    <?php endwhile; ?>
-
-                </div> <!-- /.row -->
-            </div> <!-- /.card-group -->
-
-            <!-- Pagination -->
-            <?php get_template_part('template-parts/posts-pagination'); ?>
-            <!-- Pagination End -->
-
-            <?php else: ?>
-                <?php get_template_part('template-parts/content', 'none'); ?>
-            <?php endif; ?>
-        </div> <!-- /.col-md-9 content -->
-            <!-- End Of Posts Loop -->
-
-            <?php get_sidebar('primary-sidebar'); ?>
-
-    </div> <!-- /.row -->
-</div> <!-- /.container p-4 -->
+<div class="container">
+    <!-- Posts Loop Start -->
+    <?php if ( have_posts() ) : ?>
+    
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part('template-parts/content', 'single'); ?>
+        <?php endwhile; ?>
+        <?php else : ?>
+            <?php get_template_part('template-parts/content', 'none'); ?>
+    <?php endif; ?>
+    <!-- End Of Posts Loop -->
+</div>
 
 
 
